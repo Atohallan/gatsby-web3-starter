3e0b2fc7821423cce251749f8d0bac9afc43b351
redhair <thomasbonanni@afito.com>
2021-11-14 19:58:13 -0500

    fix(layout): fixed import case bug

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Helmet from "react-helmet"
 import PropTypes from "prop-types"
-import "./Layout.css"
+import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
